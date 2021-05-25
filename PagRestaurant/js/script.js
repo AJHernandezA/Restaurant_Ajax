c1c96bc7e0f871dc3ce55ec4d9dae85e8aebbe17
Adolfo Hernandez Aponte <77458285+AJHernandezA@users.noreply.github.com>
2021-05-24 22:44:19 -0500

    Delete PagRestaurant directory

--- PagRestaurant/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $("#navbrToggle").blur(function(event){
-        var screenWidth = window.innerWidth;
-        if (screenWidth < 991){
-            $("#navbarNav").collapse('hide');
-        }
-    });
-});
